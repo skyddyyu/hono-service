0abef27d1961d80d7a900644e7342ed79f5d05e0
kaiwen <skyddyyu@outlook.com>
2024-04-21 11:55:04 +0800

    :sparkles: 初始化项目

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import {serve} from '@hono/node-server'
+import {Hono} from 'hono'
+import prisma from "../lib/prisma";
+import dayjs from "dayjs";
+
+const app = new Hono()
+
+app.get('/', (c) => {
+    return c.text('Hello Hono!')
+})
+app.post('/record', async (c) => {
+    const body = await c.req.json();
+    const record = await prisma.record.create({
+        data: {...body, createTime: dayjs()}
+    })
+    console.log('请求接受到的数据：', body)
+    console.log('保存的数据：', record)
+    return c.json({
+        tip: '接收到的数据',
+        data: body,
+    })
+})
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+    fetch: app.fetch,
+    port
+})
